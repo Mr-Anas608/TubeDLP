0dd51a15c96d72aa97e07797072e1ed46d192468
Mr. Anas <muhammad.anas.yaseen.s608@gmail.com>
2025-01-09 23:15:37 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -174,7 +174,8 @@ def get_download_options(url):
     options = {
     'no_warnings': True,
     'socket_timeout': 120,
-    'cookies': 'cookies.txt',
+    'cookiefile': './cookies.txt',  
+    'cookiesfrombrowser': ('chrome',), 
     'http_headers': {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.115 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
